d3feb44c6151d409f0718ba7fad23d682e661a2e
Vadim Firulyov <firulvv@mail.ru>
2024-10-17 22:37:44 +0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/shared/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>asdasd</Button>
+    </div>
+  );
+}
